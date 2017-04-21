29eb273bfcedde4ffe38b7ba35941eae836a4771
Daniel McDonald <mcdonadt@colorado.edu>
2017-04-21 11:57:28 -0700

    reverting unintentional changes

--- knimin/lib/qiita_jira_util.py
+++ knimin/lib/qiita_jira_util.py
@@ -15,7 +15,7 @@ from shutil import copy
 
 from tornado.escape import json_encode
 
-#from knimin import jira_handler, qiita_client, db, config
+from knimin import jira_handler, qiita_client, db, config
 from knimin import qiita_client, db, config
 from knimin.lib.format import write_sample_sheet
 
